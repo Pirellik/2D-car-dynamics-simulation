097b02075cce4addfcfac3aa6b044d7a1ee49e5e
Maciej Stepien <maciek1284@outlook.com>
2018-10-28 13:24:29 +0100

    wczytywanie mapy

--- run.py
+++ run.py
@@ -3,6 +3,26 @@ from math import copysign
 from car_drawer import CarDrawer
 from car_model import Car
 from pygame.math import Vector2
+import xml.etree.ElementTree as ET
+
+CIRCLE_TAG_NAME = '{http://www.w3.org/2000/svg}circle'
+
+def circleToPoint(circle):
+    const = 5
+    return (float(circle.attrib['cx'])*const, float(circle.attrib['cy'])*const)
+
+def readSVGFile(SVGFileName):
+    return ET.parse(SVGFileName)
+
+def getAllPoints(tree, style):
+    circles = []
+    for circle in tree.iter(CIRCLE_TAG_NAME):
+        if circle.attrib['style'] == style:
+            circles.append(circleToPoint(circle))
+
+    return circles
+    #return [circleToPoint(circle)
+    #        for circle in tree.iter(CIRCLE_TAG_NAME)]
 
 class Game:
     def __init__(self):
@@ -14,6 +34,14 @@ class Game:
         self.clock = pygame.time.Clock()
         self.ticks = 60
         self.exit = False
+        tree = readSVGFile('./track2.svg')
+
+        styleRed = "fill:#000000;stroke:none"
+        self.trackPointsRight = getAllPoints(tree, styleRed)
+        styleBlack = "fill:#ff0000;stroke:none"
+        self.trackPointsLeft = getAllPoints(tree, styleBlack)
+        print(self.trackPointsRight)
+        print(self.trackPointsLeft)
 
     def run(self):
         car = Car(0, 0)
@@ -79,6 +107,8 @@ class Game:
             pos.y = pos.y % 480
 
             car_drawer.draw(self.screen, pos, -car.angle, car.steering)
+            pygame.draw.polygon(self.screen, (0, 255, 0), (self.trackPointsLeft), 2)
+            pygame.draw.polygon(self.screen, (255, 255, 0), (self.trackPointsRight), 2)
             pygame.display.flip()
             self.clock.tick(self.ticks)
         pygame.quit()
